950c78f337011f963b4c04f5ed0156c05c30a331
Remees Salam <remeessalam@Remeess-Air.bbrouter>
2023-04-25 16:59:50 +0530

    mongoose connected and server is listening

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 const http = require('http');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const server = http.createServer(app);
+require('dotenv').config()
+const path = require('path')
+app.use(express.static('public'));
 
-http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'application/json'});
-    res.write('hello world')
-    res.end()
-}).listen(3000)
+//connection to mongoose server
+mongoose.connect(process.env.MONGOURL)
+    .then(() => {
+        console.log('mongoose is connected');
+    })
+    .catch((err) => {
+        console.log(process.env.MONGOURL)
+        console.log(`find an a error ${err}`)
+    })
+    
+    app.get('/', (req, res) => {
+        res.send('<h1 style="color:red;">hello world</h1>');    
+    })
+    
+    
+    
+    // server listening on port
+    const PORT = process.env.PORT || 3000
+    server.listen(PORT, () => {
+        console.log(`server listening on port ${PORT}`)
+    })
+    
+    
+    
+    
+    
+    
+// app.use(express.static(__dirname + '/public'));
+// process.env.MONGOURL
+//  const uri = "mongodb+srv://remeessalam:surumiremees1@cluster0.6ncimah.mongodb.net/newInstaChat?retryWrites=true&w=majority"
